f17a20f23d755e63f3c42cad3367007efa0a70d0
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2020-09-17 19:19:30 +0200

    Fix the color helper name (collided with collections)

--- lib/V073.pm
+++ lib/V073.pm
@@ -73,8 +73,8 @@ sub _token_helper ($self) {
 
 sub _other_helpers ($self) {
 
-	# MaterializeCSS color name, c(2) is the 2nd, default is c(1)
-	$self->helper(c => sub {$self->config('design_colors')->[($_[1]//1)-1]});
+	# MaterializeCSS color name, col(2) is the 2nd, default for col is col(1)
+	$self->helper(col => sub {$self->config('design_colors')->[($_[1]//1)-1]});
 
     # Render percent from [0,1]
     $self->helper(percent => sub {sprintf '%.2f %%' => $_[1] * 100});
